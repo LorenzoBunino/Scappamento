95eaf3e5d0eece7d1a5b1d18f021352f4d371c54
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-06 10:57:06 +0200

    Initial commit
    No handling of missing config file.

---
+++ Fender.py
@@ -0,0 +1,135 @@
+import os
+import configparser
+from requests import session
+from selenium import webdriver  # needs chromedriver.exe
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import pandas as pd
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+# Event handler for specs_file watchdog
+class FileCreationHandler(FileSystemEventHandler):
+    # pass the observer to the handler, so that the former can be stopped by the latter
+    def __init__(self, target_file, observer):
+        self.target_file = target_file
+        self.observer = observer
+        self.found_file = ''
+
+    def on_any_event(self, event):
+        pass
+
+    def on_created(self, event):
+        if self.target_file in event.src_path and not event.src_path.endswith('.crdownload'):
+            self.found_file = event.src_path  # annotate file
+            self.observer.stop()  # stop watching
+
+    def on_deleted(self, event):
+        pass
+
+    def on_modified(self, event):
+        if self.target_file in event.src_path and not event.src_path.endswith('.crdownload'):
+            self.found_file = event.src_path  # annotate file
+            self.observer.stop()  # stop watching
+
+    def on_moved(self, event):
+        pass
+
+
+def __main__():
+    print('-- Fender --')
+
+    # CREDENTIALS & URLs
+    config = configparser.ConfigParser()
+    with open('Fender.ini') as f:
+        config.read_file(f)
+
+        email = config['dealer.fender.com']['email']
+        password = config['dealer.fender.com']['password']
+        login_url = config['dealer.fender.com']['login_url']
+        product_filtered_list_url = config['dealer.fender.com']['product_filtered_list_url']
+
+        xlsx_inventory_filename = config['ReadyPro']['xlsx_inventory_filename']
+        csv_inventory_filename = config['ReadyPro']['csv_inventory_filename']
+        xlsx_specs_filename_partial = config['ReadyPro']['xlsx_specs_filename_partial']
+        csv_specs_filename = config['ReadyPro']['csv_specs_filename']
+        final_path = config['ReadyPro']['final_path']
+
+    options = webdriver.ChromeOptions()
+    prefs = {'download.default_directory': final_path,
+             'download.prompt_for_download': False,
+             'download.directory_upgrade': True}
+    options.add_experimental_option('prefs', prefs)
+    with webdriver.Chrome(options=options) as driver:
+        # LOGIN
+        print('Logging in...')
+
+        driver.get(login_url)
+
+        email_input = driver.find_element(By.ID, 'emailInput')
+        email_input.send_keys(email)
+
+        pass_input = driver.find_element(By.ID, 'passwordInput')
+        pass_input.send_keys(password)
+
+        login_butt = driver.find_element(By.ID, 'submitLoginButton')
+        login_butt.click()
+
+        excel_inventory_button = WebDriverWait(driver, timeout=4000) \
+            .until(ec.element_to_be_clickable((By.ID, 'inventoryDownloadButton')))
+        excel_inventory_url = excel_inventory_button.get_attribute('href')
+
+        # SWITCH TO REQUESTS / DOWNLOAD INVENTORY
+        with session() as s:
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                     "Chrome/44.0.2403.157 Safari/537.36 "
+                       }
+            s.headers.update(headers)
+            for cookie in driver.get_cookies():
+                c = {cookie['name']: cookie['value']}
+                s.cookies.update(c)
+
+            print('Downloading inventory...')
+            r = s.get(excel_inventory_url)
+            with open(final_path + xlsx_inventory_filename, 'wb') as fd:
+                for chunk in r.iter_content(chunk_size=128):
+                    fd.write(chunk)
+
+            # BACK TO SELENIUM / DOWNLOAD FANCY DATA
+            print('Downloading specs...')
+            driver.get(product_filtered_list_url)
+            excel_dropdown_button = WebDriverWait(driver, timeout=4000) \
+                .until(ec.element_to_be_clickable((By.ID, 'toggleDownloadsButton')))
+            excel_dropdown_button.click()
+            excel_specs_button = driver.find_element(By.ID, 'exportSpecsButton')
+            excel_specs_button.click()
+
+            # POLL DIR
+            observer = Observer()
+            event_handler = FileCreationHandler(xlsx_specs_filename_partial, observer)
+            observer.schedule(event_handler, final_path, recursive=False)
+            observer.start()
+            observer.join()
+            excel_specs_filename = event_handler.found_file  # absolute path, not just dir name
+
+            # LOGOUT
+            logout_dropdown_button = driver.find_element(By.CLASS_NAME, 'dropdown-toggle')
+            logout_dropdown_button.click()
+            logout_button = driver.find_element(By.CSS_SELECTOR, '.dropdown-menu>li>a>i.fa-sign-out')
+            logout_button.click()
+
+    # FILE CONVERSION
+    inventory_list_xlsx = pd.read_excel(final_path + xlsx_inventory_filename, header=None)
+    inventory_list_xlsx.to_csv(final_path + csv_inventory_filename, sep=';', header=None, index=False, encoding='utf-8')
+    os.remove(final_path + xlsx_inventory_filename)
+
+    specs_list_xlsx = pd.read_excel(excel_specs_filename, header=None)
+
+    specs_list_xlsx.to_csv(final_path + csv_specs_filename, sep=';', header=None, index=False, encoding='utf-8')
+    os.remove(excel_specs_filename)
+
+
+if __name__ == '__main__':
+    __main__()
