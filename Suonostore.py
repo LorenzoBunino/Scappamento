c3f76281be8fdf4926065df5acfe2747d472099a
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-07 13:28:20 +0200

    Revamp error messages, clean code
    - Output unfixed errors and details
    - simplify error tracking, reduce variables

--- Suonostore.py
+++ Suonostore.py
@@ -17,9 +17,10 @@ def __main__():
         csv_filename = config['ReadyPro']['csv_filename']
         final_path = config['ReadyPro']['final_path']
 
+    # DOWNLOAD
     with Session() as s:
         print('Downloading with auth...')
-        # SITE USES HTTP Basic Auth
+        # Site uses HTTP Basic Auth
         r = s.get(csv_url, auth=(user, password), headers={'User-Agent': 'Chrome'})
         with open(final_path + csv_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=128):
@@ -27,41 +28,37 @@ def __main__():
 
     # CLEANUP: NUMBERS,SEPARATORS, DATES, <INCHES> SYMBOLS
     sep = ';'
-    problematic_count = 0
+    problematic_field_count = 0
     with open(final_path + csv_filename, 'r', encoding="Latin-1") as f:
         new_csv = ''
 
         line_count = 0
-        is_first_line = True
+        problematic_line_count = 0
+        fixed_problematic_line_count = 0
         for line in f:
 
-            if is_first_line:  # skip CSV header
+            if not line_count:  # skip first line = CSV header
                 new_csv = line
-                is_first_line = False
+                line_count = line_count + 1
                 continue
-            line_count = line_count + 1
 
             temp_line = line.replace('00:00:00', '')
             temp_line = temp_line.replace('.00000,', ',')
             temp_line = temp_line.replace('.000,', ',')
             temp_line = temp_line.replace('.00,', ',')
-            # temp_line = temp_line.replace('000', '')
             temp_line = temp_line.replace('/  /', '')
             temp_line = temp_line.replace(',', sep)
 
             rebuilt_temp_line = ''
-            is_first_field = True
-            is_second_field = False
             field_count = 0
+            found_problematic_field = False
             for field in temp_line.split(';'):  # for each field in line, look for double quotes as <inches> symbols
-                field_count = field_count + 1
 
-                if is_second_field:
+                if field_count == 1:  # if second field
                     temp_cod_art = field
-                    is_second_field = False
 
                 if field.count('"') % 2:  # if current field contains an uneven amount of double quotes
-
+                    found_problematic_field = True
                     match = False
                     for i in range(len(field)-1, -1, -1):  # for each char in field, inverted, greedy [0-9]" match
                         if i and field[i] == '"' and field[i-1].isdigit():
@@ -70,22 +67,31 @@ def __main__():
                             break  # greedy
 
                     if not match:  # problematic fields are copied as-is for now
-                        print('⚠ [ Row ', line_count, '][', temp_cod_art, ']', 'Uh oh: field ', field_count)
-                        problematic_count = problematic_count + 1
+                        print('⚠ [ Row ', line_count, '][', temp_cod_art, ']', 'Uh oh: field ', field_count + 1)
+                        problematic_field_count = problematic_field_count + 1
                         rebuilt_temp_line = rebuilt_temp_line + sep + field
 
                 else:
-                    if is_first_field:
+                    if not field_count:
                         rebuilt_temp_line = field
-                        is_first_field = False
-                        is_second_field = True
                     else:
                         rebuilt_temp_line = rebuilt_temp_line + sep + field
 
-            # new_csv = new_csv + temp_line
-            new_csv = new_csv + rebuilt_temp_line
+                field_count = field_count + 1
 
-    print('⚠ ', problematic_count, ' problematic lines')
+            if found_problematic_field:
+                if match:
+                    fixed_problematic_line_count = fixed_problematic_line_count + 1
+                problematic_line_count = problematic_line_count + 1
+
+            new_csv = new_csv + rebuilt_temp_line
+            line_count = line_count + 1
+
+    if problematic_field_count:
+        print('⚠ ', problematic_field_count, ' problematic field', '' if problematic_field_count < 2 else 's',
+              ' in ', problematic_line_count - fixed_problematic_line_count, ' problematic line',
+              '' if problematic_line_count < 2 else 's', ' (', problematic_line_count, ' lines total, ',
+              fixed_problematic_line_count, ' fixed)', sep='')
 
     with open(final_path + csv_filename, 'w', encoding="utf-8") as f:
         f.write(new_csv)
