25ac59e36482f243238897c015617042a19437cd
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-07 11:03:38 +0200

    Fix (partially) double quote parity check
    - Check double quotes parity
    - Only fix double quotes next to digits (used as <inches>)
    - Copy over all other parity failures as-is

--- Suonostore.py
+++ Suonostore.py
@@ -27,9 +27,20 @@ def __main__():
 
     # CLEANUP: NUMBERS,SEPARATORS, DATES, <INCHES> SYMBOLS
     sep = ';'
+    problematic_count = 0
     with open(final_path + csv_filename, 'r', encoding="Latin-1") as f:
-        new_csv = ""
+        new_csv = ''
+
+        line_count = 0
+        is_first_line = True
         for line in f:
+
+            if is_first_line:  # skip CSV header
+                new_csv = line
+                is_first_line = False
+                continue
+            line_count = line_count + 1
+
             temp_line = line.replace('00:00:00', '')
             temp_line = temp_line.replace('.00000,', ',')
             temp_line = temp_line.replace('.000,', ',')
@@ -39,32 +50,43 @@ def __main__():
             temp_line = temp_line.replace(',', sep)
 
             rebuilt_temp_line = ''
-            first = True
+            is_first_field = True
+            is_second_field = False
+            field_count = 0
             for field in temp_line.split(';'):  # for each field in line, look for double quotes as <inches> symbols
+                field_count = field_count + 1
+
+                if is_second_field:
+                    temp_cod_art = field
+                    is_second_field = False
 
                 if field.count('"') % 2:  # if current field contains an uneven amount of double quotes
 
                     match = False
                     for i in range(len(field)-1, -1, -1):  # for each char in field, inverted, greedy [0-9]" match
                         if i and field[i] == '"' and field[i-1].isdigit():
-                            rebuilt_temp_line = rebuilt_temp_line + sep + field[0:i-1] + '″' + field[i+1:len(field)-1]
-                            # TODO: change to sep + field[0:i] + '″' + field[i+1:len(field)]
+                            rebuilt_temp_line = rebuilt_temp_line + sep + field[0:i] + '″' + field[i+1:len(field)]
                             match = True
                             break  # greedy
 
-                    if not match:
-                        print('Uh oh, found a problematic one')
-                        pass  # what do?
+                    if not match:  # problematic fields are copied as-is for now
+                        print('⚠ [ Row ', line_count, '][', temp_cod_art, ']', 'Uh oh: field ', field_count)
+                        problematic_count = problematic_count + 1
+                        rebuilt_temp_line = rebuilt_temp_line + sep + field
 
                 else:
-                    if first:
+                    if is_first_field:
                         rebuilt_temp_line = field
-                        first = False
+                        is_first_field = False
+                        is_second_field = True
                     else:
                         rebuilt_temp_line = rebuilt_temp_line + sep + field
 
             # new_csv = new_csv + temp_line
             new_csv = new_csv + rebuilt_temp_line
+
+    print('⚠ ', problematic_count, ' problematic lines')
+
     with open(final_path + csv_filename, 'w', encoding="utf-8") as f:
         f.write(new_csv)
 
