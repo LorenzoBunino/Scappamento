824b56564d7a9e06e93f5ad7a7a7cc7ed6040c36
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-07 17:34:17 +0200

    Add TODO, placeholder code

--- MusicPool.py
+++ MusicPool.py
@@ -11,8 +11,15 @@ def __main__():
         user = config['musicpool.it']['user']
         password = config['musicpool.it']['password']
 
+    if user:  # placeholder
+        pass
+
     with session() as s:
         s.get('musicpool.it')  # placeholder
+        # TODO: get excel link from DOM
+        #  example: <a href="http://www.mediafire.com/file/vy6rtdib4v32tc4/maga_arca.xlsx/fileC" title="DOWNLOAD"
+        #  rel="nofollow" class="style_button_wrap"><span class="btn style_button style_button_0 btn-danger
+        #  adveditor_curr">DOWNLOAD</span></a>
 
 
 if __name__ == '__main__':
