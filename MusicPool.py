88cdca46516a2fa0c3ecd7ed5b8cc070b852ca6b
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-09 16:57:51 +0200

    Convert output file to CSV
    Use pandas to download the excel file directly into a dataframe

--- MusicPool.py
+++ MusicPool.py
@@ -6,6 +6,7 @@ import configparser
 from requests import session
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
 
 
 def __main__():
@@ -27,16 +28,24 @@ def __main__():
         payload = {'email': email, 'passwd': password, 'SubmitLogin': ''}
         s.post(form_action_url, data=payload, headers={'User-Agent': 'Chrome'})
 
-        # Parsing
+        # Parse
         musicpool_soup = BeautifulSoup(s.get(login_url).text, 'html.parser')
         intermediate_url = musicpool_soup.find(text='DOWNLOAD').parent.parent['href']
 
         external_host_soup = BeautifulSoup(s.get(intermediate_url).text, 'html.parser')
         # mark regular expression as a raw string to prevent <invalid unicode escape sequence> errors
         xlsx_url = external_host_soup.find(text=re.compile(r'Download[\s]*\([\d]+[.]?[\d]*[A-Z][A-Z]\)')).parent['href']
-        r = s.get(xlsx_url)
-
-        pass  # manipulate and save
+        # r = s.get(xlsx_url)
+        #
+        # # Manipulate and save
+        # print(r.encoding)
+        # chunks = []
+        # for chunk in r.iter_content(chunk_size=128):
+        #     chunks.append(chunk)
+        # encoding = r.encoding
+        # xlsx_file = b''.join(chunks).decode(encoding if encoding is not None else 'Latin-1')
+        # pd.read_excel(xlsx_file, header=None).to_csv('C:\\Ready\\ReadyPro\\Archivi\\testPool.csv', sep=';', index=False)
+        pd.read_excel(xlsx_url, header=None).to_csv('C:\\Ready\\ReadyPro\\Archivi\\testPool.csv', sep=';', index=False)
 
 
 if __name__ == '__main__':
