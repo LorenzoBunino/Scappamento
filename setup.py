3df6bad523c07c74c1955c87d4012677d549f154
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-14 09:44:34 +0200

    Initial commit, incomplete

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='scappamento',
+    version='0.1a1',
+    author='Lorenzo Bunino',
+    author_email="bunino.lorenzo@gmail.com"
+    # TODO: add arguments
+)
