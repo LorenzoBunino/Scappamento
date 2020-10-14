36d052c54d09b4b9c62d654b820b5d8a6f9f468f
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-14 16:03:51 +0200

    Initial commit
    Provide a common general function for config retrieval.

---
+++ scappamento/_common.py
@@ -0,0 +1,19 @@
+# Common functionality to be shared across modules
+# This is not a stand-alone module
+
+
+import configparser
+
+
+def get_config(name, key_list_str, path):
+    config = configparser.ConfigParser()
+
+    with open(path) as f:
+        config.read_file(f)
+
+        key_list_str = key_list_str.split(', ')
+        val_list = []
+        for key in key_list_str:
+            val_list.append(config[name][key])
+
+    return val_list
