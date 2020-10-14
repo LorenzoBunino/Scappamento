ad7e48eb86593989b6884212c4d51b8e0b66b582
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-14 16:01:49 +0200

    Fill-in argument list and info
    File now contains correct information and is in a usable state.

--- setup.py
+++ setup.py
@@ -5,8 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='scappamento',
-    version='0.1a1',
+    version='0.1a1',  # TODO: decide first version identifier
     author='Lorenzo Bunino',
-    author_email="bunino.lorenzo@gmail.com"
-    # TODO: add arguments
+    author_email="bunino.lorenzo@gmail.com",
+    description="B2B automation for music stores",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lorenzobunino/scappamento",
+    packages=setuptools.find_packages(),  # TODO: find_packages vs hand-compiled list
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",  # TODO: fix path handling and
+                                                     #  change "Microsoft :: Windows" to "OS Independent"
+    ],
+    python_requires='>=3.6'
 )
