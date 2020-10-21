c9c4fc469cd5390b1f8975eed52d99da5d719a97
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-21 10:47:02 +0200

    Feat: add descriptive prints during execution

--- scappamento/frenexport.py
+++ scappamento/frenexport.py
@@ -42,16 +42,21 @@ def __main__():
     # Database connection and query
     print('Connecting to database...')
     conn = msq.connect(host=host, database=database, user=user, password=password)
+    print('Querying...')
     results = pandas.read_sql_query(query, conn)
     conn.close()
 
     # Result cleanup: separator, fields
+    print('Cleaning up...')
     results_clean = results.replace(';', ',', regex=True)  # replace instances of to-be CSV separator character
 
+    edited_row_count = 0
+    edited_field_count = 0
     for i in range(0, len(results.index)):  # clean product "pretty name", remove instances of other fields' content
         des_art_temp = results_clean.at[i, 'DES_ART']  # cache fields instead of accessing dataframe x times
         marca_temp = results_clean.at[i, 'MARCA'].lower()  # lower() is tailored to query file currently in use
         modello_temp = results_clean.at[i, 'MODELLO'].lower()
+        edited_field_count = edited_field_count + 1
 
         if marca_temp and marca_temp in des_art_temp:
             des_art_temp = des_art_temp.replace(' ' + marca_temp, '')  # update field for later checks
