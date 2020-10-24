a1cba2a30f018b6ad8126d6ad97d6ca1661a5496
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-24 17:00:23 +0200

    Feat: [Functional] copy data from generated Excel file to properly formatted Excel file
    Copy data from database-generated Excel doc to mepa-downloaded Excel doc.

--- scappamento/mepa.py
+++ scappamento/mepa.py
@@ -1,7 +1,8 @@
 # TODO: description header
 
 from .supplier import Supplier, ScappamentoError
-import pandas as pd
+from xlrd import open_workbook
+from xlutils.copy import copy
 
 
 def update():
@@ -12,47 +13,31 @@ def update():
 
     # Config
     config_path = 'C:\\Ready\\ReadyPro\\Archivi\\scappamento.ini'
-    key_list = ['excel_filename',
-                'ref_excel_filename']
+    key_list = ['readypro_excel_filename',
+                'mepa_excel_filename']
 
     mepa.load_config(key_list, config_path)
 
-    [excel_filename,
-     ref_excel_filename] = mepa.val_list
+    [readypro_excel_filename,
+     mepa_excel_filename] = mepa.val_list
 
     # Do stuff
-
-    xls = pd.read_excel(excel_filename, header=None)
+    # xls = pd.read_excel(excel_filename, header=None)
     'Access||Accu Case||Acorn||ADAM Professional Audio'
 
-    if 'field' not in xls:
-        pass
+    readypro_xls = open_workbook(readypro_excel_filename)
+    mepa_xls_model = open_workbook(mepa_excel_filename, formatting_info=True)
 
-    # TODO: properly generate file from Ready Pro, start chipping away at file issues
+    mepa_xls_new = copy(mepa_xls_model)
 
-    # # CREDENTIALS AND URLs
-    # config = configparser.ConfigParser()
-    # with open('MEPA.ini') as f:
-    #     config.read_file(f)
-    #
-    #     user = config['acquistinretepa.it']['user']
-    #     password = config['acquistinretepa.it']['password']
-    #     login_url = config['acquistinretepa.it']['login_url']
-    #
-    # with session() as s:
-    #     s.get(login_url)
-    #
-    # # PACKAGE ASSEMBLY IMMUTABLE
-    #
-    # conn = msq.connect(host=host, database=database, user=user, password=password)
-    # with open(final_path + sql_filename) as f:
-    #     query = f.read()
-    #
-    # results = pandas.read_sql_query(query, conn)
-    #
-    # results.to_csv(final_path + csv_filename, sep=';', index=False)
-    #
-    # conn.close()
+    mepa_inst_sheet = mepa_xls_new.get_sheet(1)
+    readypro_inst_sheet = readypro_xls.sheet_by_index(0)
+
+    for i in range(1, 6):  # TODO: remove limits, test on file bounds
+        for j in range(0, 20):
+            mepa_inst_sheet.write(i, j, readypro_inst_sheet.cell_value(i, j))
+
+    mepa_xls_new.save('C:\\Ready\\ReadyPro\\Archivi\\newmepatest.xls')
 
 
 if __name__ == '__main__':
