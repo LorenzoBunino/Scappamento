ccb977e02a66846b735b4a8403b1075449074eaa
Lorenzo <bunino.lorenzo@gmail.com>
2020-10-28 17:32:29 +0100

    Initial commit

---
+++ tests/supplier_test.py
@@ -0,0 +1,17 @@
+# Tests for Supplier class
+# Tests for common usage functions
+
+import unittest
+
+import scappamento.supplier
+
+
+class TestSupplier(unittest.TestCase):
+    def test_constructor(self):  # TODO: config, no_config
+        pass
+
+    def test_fix_illegal_sep_quotes(self):
+        pass
+
+    def test_switch_sep(self):
+        pass
