8bc18af08b220bd92ca57eeb7529eff825201880
Lorenzo <bunino.lorenzo@gmail.com>
2020-11-05 12:47:58 +0100

    Fix: handle file name conflict error
    Delete old backup file before renaming initial file to its name.

--- scappamento/mogar.py
+++ scappamento/mogar.py
@@ -21,6 +21,9 @@ def update():
     csv_file_path = os.path.join(target_path, csv_filename)
     csv_bak_path = csv_file_path + '.bak'
 
+    if os.path.isfile(csv_bak_path):
+        os.remove(csv_bak_path)
+
     os.rename(csv_file_path, csv_bak_path)
 
     with open(csv_bak_path, 'r', encoding='utf_16_le') as old_f:
