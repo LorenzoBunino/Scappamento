5d299ba2d663660a8b97f2f0b6f18af28838c620
LorenzoBunino <bunino.lorenzo@gmail.com>
2020-11-11 18:16:22 +0100

    Feat: prep for initial mepa release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='scappamento',
-    version='0.1a5',
+    version='0.1a6',
     author='Lorenzo Bunino',
     author_email="bunino.lorenzo@gmail.com",
     description="B2B automation for music stores",
