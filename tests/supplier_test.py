35ac292361f12a651e647268dcb9dda962070e5c
LorenzoBunino <bunino.lorenzo@gmail.com>
2020-11-12 16:02:33 +0100

    Feat: implement tests for CSV line fix functions
    Implement test for switch_sep().
    Implement test for fix_illegal_sep_quotes().

--- tests/supplier_test.py
+++ tests/supplier_test.py
@@ -3,7 +3,7 @@
 
 import unittest
 
-import scappamento.supplier
+import scappamento.supplier as sc
 
 
 class TestSupplier(unittest.TestCase):
@@ -11,7 +11,19 @@ class TestSupplier(unittest.TestCase):
         pass
 
     def test_fix_illegal_sep_quotes(self):
-        pass
+        line = '"asd";"hehehehe;onetwothree";";hihiha";"hahahu;";";hohoho;"'
+        sep = ';'
+        rep = ','
+        line_fixed = '"asd";"hehehehe,onetwothree";",hihiha";"hahahu,";",hohoho,"'
+        self.assertEqual(sc.fix_illegal_sep_quotes(line, sep, rep), line_fixed, 'Should convert separators')
 
     def test_switch_sep(self):
-        pass
+        line = '"asd";"hehehehe,onetwothree";",hihiha";"hahahu,";",hohoho,"'
+        sep_old = ';'
+        sep_new = ','
+        line_switched = '"asd","hehehehe;onetwothree",";hihiha","hahahu;",";hohoho;"'
+        self.assertEqual(sc.switch_sep(line, sep_old, sep_new), line_switched, 'Should switch separators')
+
+
+if __name__ == '__main__':
+    unittest.main()
